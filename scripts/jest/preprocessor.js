f3ff614468d5a28e6ee79a10565fad0c1699d07d
Dylan Qian <dylan.qian.29@gmail.com>
2019-02-01 13:47:53 -0800

    update to React14

--- scripts/jest/preprocessor.js
+++ scripts/jest/preprocessor.js
@@ -1,76 +1,75 @@
 'use strict';
 
-const path = require('path');
+// React's test can only work in NODE_ENV=test because of how things
+// are set up. So we might as well enforce it.
+process.env.NODE_ENV = 'test';
 
-const babel = require('babel-core');
-const coffee = require('coffee-script');
+var path = require('path');
 
-const tsPreprocessor = require('./typescript/preprocessor');
-const createCacheKeyFunction = require('fbjs-scripts/jest/createCacheKeyFunction');
+var assign = require('object-assign');
+var babel = require('babel');
+var coffee = require('coffee-script');
 
-// Use require.resolve to be resilient to file moves, npm updates, etc
-const pathToBabel = path.join(
-  require.resolve('babel-core'),
-  '..',
-  'package.json'
-);
-const pathToBabelPluginDevWithCode = require.resolve(
-  '../error-codes/replace-invariant-error-codes'
-);
-const pathToBabelPluginWrapWarning = require.resolve(
-  '../babel/wrap-warning-with-env-check'
-);
-const pathToBabelPluginAsyncToGenerator = require.resolve(
-  'babel-plugin-transform-async-to-generator'
-);
-const pathToBabelrc = path.join(__dirname, '..', '..', '.babelrc');
-const pathToErrorCodes = require.resolve('../error-codes/codes.json');
+var tsPreprocessor = require('./ts-preprocessor');
 
-const babelOptions = {
-  plugins: [
-    // For Node environment only. For builds, Rollup takes care of ESM.
-    require.resolve('babel-plugin-transform-es2015-modules-commonjs'),
+// This assumes the module map has been built. This might not be safe.
+// We should consider consuming this from a built fbjs module from npm.
+var moduleMap = require('fbjs/module-map');
+var babelPluginDEV = require('fbjs-scripts/babel/dev-expression');
+var babelPluginModules = require('fbjs-scripts/babel/rewrite-modules');
+var createCacheKeyFunction = require('fbjs-scripts/jest/createCacheKeyFunction');
 
-    pathToBabelPluginDevWithCode,
-    pathToBabelPluginWrapWarning,
+// Use require.resolve to be resiliant to file moves, npm updates, etc
+var pathToBabel = path.join(require.resolve('babel'), '..', 'package.json');
+var pathToModuleMap = require.resolve('fbjs/module-map');
+var pathToBabelPluginDev = require.resolve('fbjs-scripts/babel/dev-expression');
+var pathToBabelPluginModules = require.resolve('fbjs-scripts/babel/rewrite-modules');
 
-    // Keep stacks detailed in tests.
-    // Don't put this in .babelrc so that we don't embed filenames
-    // into ReactART builds that include JSX.
-    // TODO: I have not verified that this actually works.
-    require.resolve('babel-plugin-transform-react-jsx-source'),
-
-    require.resolve('../babel/transform-prevent-infinite-loops'),
+// TODO: make sure this stays in sync with gulpfile
+var babelOptions = {
+  nonStandard: true,
+  blacklist: [
+    'spec.functionName',
+    'validation.react',
   ],
+  optional: [
+    'es7.trailingFunctionCommas',
+  ],
+  plugins: [babelPluginDEV, babelPluginModules],
   retainLines: true,
+  _moduleMap: moduleMap,
 };
 
 module.exports = {
   process: function(src, filePath) {
     if (filePath.match(/\.coffee$/)) {
-      return coffee.compile(src, {bare: true});
+      return coffee.compile(src, {'bare': true});
     }
     if (filePath.match(/\.ts$/) && !filePath.match(/\.d\.ts$/)) {
       return tsPreprocessor.compile(src, filePath);
     }
-    if (!filePath.match(/\/third_party\//)) {
-      // for test files, we also apply the async-await transform, but we want to
-      // make sure we don't accidentally apply that transform to product code.
-      const isTestFile = !!filePath.match(/\/__tests__\//);
-      return babel.transform(
-        src,
-        Object.assign(
-          {filename: path.relative(process.cwd(), filePath)},
-          babelOptions,
-          isTestFile
-            ? {
-                plugins: [pathToBabelPluginAsyncToGenerator].concat(
-                  babelOptions.plugins
-                ),
-              }
-            : {}
-        )
-      ).code;
+    if (
+      !filePath.match(/\/node_modules\//) &&
+      !filePath.match(/\/third_party\//)
+    ) {
+      var rv =
+        babel.transform(src, assign({filename: filePath}, babelOptions)).code;
+      // hax to turn fbjs/lib/foo into /path/to/node_modules/fbjs/lib/foo
+      // because jest is slooow with node_modules paths (facebook/jest#465)
+      rv = rv.replace(
+        /require\('(fbjs\/lib\/.+)'\)/g,
+        function(call, arg) {
+          return (
+            'require(' +
+            JSON.stringify(
+              path.join(__dirname, '../../node_modules', arg)
+            ) +
+            ')'
+          );
+        }
+      );
+
+      return rv;
     }
     return src;
   },
@@ -78,9 +77,8 @@ module.exports = {
   getCacheKey: createCacheKeyFunction([
     __filename,
     pathToBabel,
-    pathToBabelrc,
-    pathToBabelPluginDevWithCode,
-    pathToBabelPluginWrapWarning,
-    pathToErrorCodes,
+    pathToModuleMap,
+    pathToBabelPluginDev,
+    pathToBabelPluginModules,
   ]),
 };
